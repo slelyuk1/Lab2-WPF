96fecc85132aed45df0c6f1a94e5adbb07b79252
Oleksandr Leliuk <37149932+slelyuk1@users.noreply.github.com>
2019-03-02 23:34:03 +0200

    Add files via upload
    (cherry picked from commit a19772ee45e710da56cdcb3c273eb37cbae38583)

---
+++ Lab2_Leliuk/Lab2_Leliuk/Managers/NavigationManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab2_Leliuk.Models;
+
+namespace Lab2_Leliuk.Managers
+{
+    class NavigationManager
+    {
+        private static NavigationManager _manager;
+        private static object _lock = new object();
+        private NavigationModel _model;
+
+        public static NavigationManager Instance
+        {
+            get
+            {
+                if (_manager == null)
+                {
+                    lock (_lock)
+                    {
+                        _manager = new NavigationManager();
+                    }
+                }
+
+                return _manager;
+            }
+        }
+
+        public void Initialise(NavigationModel model)
+        {
+            _model = model;
+        }
+
+        public void Navigate(Models.Views view)
+        {
+            _model?.Navigate(view);
+        }
+    }
+}
