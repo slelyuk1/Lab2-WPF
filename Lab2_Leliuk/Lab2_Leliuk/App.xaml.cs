a19772ee45e710da56cdcb3c273eb37cbae38583
Oleksandr Leliuk <37149932+slelyuk1@users.noreply.github.com>
2019-03-02 23:34:03 +0200

    Add files via upload

---
+++ Lab2_Leliuk/Lab2_Leliuk/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Lab2_Leliuk.Managers;
+using Lab2_Leliuk.Models;
+
+namespace Lab2_Leliuk
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow mainWindow = new MainWindow();
+            Storage data = new Storage();
+            NavigationManager.Instance.Initialise(new NavigationModel(mainWindow, data));
+            NavigationManager.Instance.Navigate(Models.Views.UserInputView);
+            mainWindow.Show();
+        }
+    }
+}
---
+++ Lab2_Leliuk/Lab2_Leliuk/Models/Storage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab2_Leliuk.Models
+{
+    public class Storage
+    {
+        public event Action<Person> UserChanged;
+        private Person _user;
+
+        public Person User
+        {
+            get => _user;
+            set => _user = value;
+        }
+
+        public void ChangeUser(Person user)
+        {
+            _user = user;
+            UserChanged?.Invoke(user);
+        }
+    }
+}
