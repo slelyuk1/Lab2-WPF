3b6b0eabb388b811f31ff6fd432a3cd2e9bb94ce
Oleksandr Leliuk <37149932+slelyuk1@users.noreply.github.com>
2019-03-11 00:00:47 +0200

    Add files via upload

--- Lab2_Leliuk/Lab2_Leliuk/Models/Person.cs
+++ Lab2_Leliuk/Lab2_Leliuk/Models/Person.cs
@@ -4,13 +4,27 @@ namespace Lab2_Leliuk.Models
 {
     public class Person
     {
-        private static readonly string[] _chinezeSigns =
-            {"Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Sheep"};
+        enum ChineseSigns
+        {
+            Monkey,
+            Rooster,
+            Dog,
+            Pig,
+            Rat,
+            Ox,
+            Tiger,
+            Rabbit,
+            Dragon,
+            Snake,
+            Horse,
+            Sheep
+        }
 
         private string _name;
         private string _surname;
         private string _email;
         private DateTime _birthDate = DateTime.Now;
+        private string _chineseSign, _sunSign;
 
         public Person(string name, string surname, string email, DateTime birthDate)
         {
@@ -18,6 +32,8 @@ namespace Lab2_Leliuk.Models
             _surname = surname;
             _email = email;
             _birthDate = birthDate;
+            _chineseSign = CalculateChineseSign(_birthDate);
+            _sunSign = CalculateSunSign(_birthDate);
         }
 
         public Person(string name, string surname, string email)
@@ -32,18 +48,18 @@ namespace Lab2_Leliuk.Models
             _name = name;
             _surname = surname;
             _birthDate = birthDate;
+            _chineseSign = CalculateChineseSign(_birthDate);
+            _sunSign = CalculateSunSign(_birthDate);
         }
 
         public string Name
         {
             get => _name;
-            set => _name = value;
         }
 
         public string Surname
         {
             get => _surname;
-            set => _surname = value;
         }
 
         public string Email
@@ -55,7 +71,12 @@ namespace Lab2_Leliuk.Models
         public DateTime BirthDate
         {
             get => _birthDate;
-            set => _birthDate = value;
+            set
+            {
+                _birthDate = value;
+                _sunSign = CalculateSunSign(_birthDate);
+                _chineseSign = CalculateChineseSign(_birthDate);
+            }
         }
 
 
@@ -70,49 +91,56 @@ namespace Lab2_Leliuk.Models
 
         public string SunSign
         {
-            get
-            {
-                var day = _birthDate.Day;
-                switch (_birthDate.Month)
-                {
-                    case 1:
-                        return day < 21 ? "Capricorn" : "Aquarius";
-                    case 2:
-                        return day < 20 ? "Aquarius" : "Pisces";
-                    case 3:
-                        return day < 21 ? "Pisces" : "Aries";
-                    case 4:
-                        return day < 21 ? "Aries" : "Taurus";
-                    case 5:
-                        return day < 21 ? "Taurus" : "Gemini";
-                    case 6:
-                        return day < 21 ? "Gemini" : "Cancer";
-                    case 7:
-                        return day < 22 ? "Cancer" : "Leo";
-                    case 8:
-                        return day < 22 ? "Leo" : "Virgo";
-                    case 9:
-                        return day < 22 ? "Virgo" : "Libra";
-                    case 10:
-                        return day < 22 ? "Libra" : "Scorpio";
-                    case 11:
-                        return day < 22 ? "Scorpio" : "Sagittarius";
-                    case 12:
-                        return day < 22 ? "Sagittarius" : "Capricorn";
-                    default:
-                        throw new ArgumentException("Inappropriate format of month !");
-                }
-            }
+            get => _sunSign;
         }
 
         public string ChineseSign
         {
-            get => _chinezeSigns[_birthDate.Year % 12];
+            get => _chineseSign;
         }
 
         public bool IsBirthDay
         {
             get => _birthDate.Day == DateTime.Now.Day && _birthDate.Month == DateTime.Now.Month;
+        }
+
+        public static string CalculateSunSign(DateTime date)
+        {
+            var day = date.Day;
+            switch (date.Month)
+            {
+                case 1:
+                    return day < 21 ? "Capricorn" : "Aquarius";
+                case 2:
+                    return day < 20 ? "Aquarius" : "Pisces";
+                case 3:
+                    return day < 21 ? "Pisces" : "Aries";
+                case 4:
+                    return day < 21 ? "Aries" : "Taurus";
+                case 5:
+                    return day < 21 ? "Taurus" : "Gemini";
+                case 6:
+                    return day < 21 ? "Gemini" : "Cancer";
+                case 7:
+                    return day < 22 ? "Cancer" : "Leo";
+                case 8:
+                    return day < 22 ? "Leo" : "Virgo";
+                case 9:
+                    return day < 22 ? "Virgo" : "Libra";
+                case 10:
+                    return day < 22 ? "Libra" : "Scorpio";
+                case 11:
+                    return day < 22 ? "Scorpio" : "Sagittarius";
+                case 12:
+                    return day < 22 ? "Sagittarius" : "Capricorn";
+                default:
+                    throw new ArgumentException("Inappropriate format of month !");
+            }
+        }
+
+        public static string CalculateChineseSign(DateTime date)
+        {
+            return ((ChineseSigns)(date.Year % 12)).ToString();
         }
     }
 }
