b0ff9abf2b73ef187a69b0c60a3158495b1d207d
Oleksandr Leliuk <slelyuk1@gmail.com>
2022-01-07 20:32:54 +0200

    Fixed visualization behaviour

--- Shared/View/Visualizer/WindowViewVisualizer.cs
+++ Shared/View/Visualizer/WindowViewVisualizer.cs
@@ -14,8 +14,26 @@ namespace Shared.View.Visualizer
         public void Visualize(FrameworkElement toVisualize)
         {
             _window.Title = toVisualize.Name;
-            _window.MinHeight = toVisualize.MinHeight;
-            _window.MinWidth = toVisualize.MinWidth;
+
+            double? minHeight = double.IsNaN(toVisualize.MinHeight) || double.IsInfinity(toVisualize.MinHeight) ? null : toVisualize.MinHeight;
+            double? minWidth = double.IsNaN(toVisualize.MinWidth) || double.IsInfinity(toVisualize.MinWidth) ? null : toVisualize.MinWidth;
+            _window.MinHeight = minHeight ?? 0;
+            _window.MinWidth = minWidth ?? 0;
+
+            double? height = double.IsNaN(toVisualize.Height) || double.IsInfinity(toVisualize.Height) ? null : toVisualize.Height;
+            double? width = double.IsNaN(toVisualize.Width) || double.IsInfinity(toVisualize.Width) ? null : toVisualize.Width;
+            height ??= minHeight;
+            width ??= minHeight;
+            if (height != null)
+            {
+                _window.Height = (double) height;
+            }
+
+            if (width != null)
+            {
+                _window.Width = (double) width;
+            }
+
             _window.Content = toVisualize;
         }
     }
